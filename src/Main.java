166b053f0726337b86ef242642dc706c7fa769b3
deepak.kharol <dkharol48@gmail.com>
2024-07-13 12:32:42 +0530

    Changed the hello message

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello and welcome!");
+        System.out.print("Hello and welcome people!");
 
         for (int i = 1; i <= 6; i++) {
             System.out.println(StaticClass.VALUE_OF + StaticClass.I_EQUALS + i);
