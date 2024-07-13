d920ce5e06b2305f6daf661925ca9c953fd0bdcb
deepak.kharol <dkharol48@gmail.com>
2024-07-13 13:40:44 +0530

    temp

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello and welcome people!");
+        System.out.print("Hello and welcome!");
 
         for (int i = 1; i <= 10; i++) {
             System.out.println(StaticClass.VALUE_OF + StaticClass.I_EQUALS + i);
